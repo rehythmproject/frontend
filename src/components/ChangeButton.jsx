747e3664084925ec11779d38f421b720fb98bdac
lioba00700 <lioba00700@naver.com>
2024-09-14 23:06:13 +0900

    버튼 바뀌는거 구현

--- src/components/ChangeButton.jsx
+++ src/components/ChangeButton.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import "./ChangeButton.css"
 
 const ChangeButton = () => {
-      return (
-        <div className="change-maindiv">
-          <button className="change-button" style={{background:'white', color:'black'}}>나에 대한</button>
-          <button className="change-button">상황</button>
-        </div>
-      )
+  const [changeButton, setChangeButton] = useState('나에 대한');
+
+  const onClickChangeButton = (e) => {
+    setChangeButton(e.target.textContent);
+  }
+  return (
+    <div className="change-maindiv">
+      <button className="change-button" style={{backgroundColor: changeButton==='나에 대한' &&'white', color:changeButton==='나에 대한' &&'black'}} onClick={onClickChangeButton}>나에 대한</button>
+      <button className="change-button" style={{backgroundColor: changeButton==='상황' &&'white', color:changeButton==='상황' &&'black'}} onClick={onClickChangeButton}>상황</button>
+    </div>
+  )
 }
 
 export default ChangeButton;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import './Main.css';
 import RecentModelItem from './RecentModelItem';
 import ModelItem from './ModelItem';
-
-const tagName = [
-    '업무','공부','회의','회의','회의'
-];
+import { useState } from 'react';
 
 const Main = () => {
+    const [category, setCategory] = useState('공부');
+    const onClickCategory = (e) => {
+        setCategory(e.target.textContent);
+    }
     return(
         <div className="main">
             <h3 className='main_title'>최근 사용한 기능</h3>
@@ -17,9 +18,9 @@ const Main = () => {
                 <li><RecentModelItem/></li>
             </ul>
             <div className='category_container'>
-                <button className='category' style={{backgroundColor:'white', color:'black'}}>공부</button>
-                <button className='category'>업무</button>
-                <button className='category'>회의</button>
+                <button className='category' style={{backgroundColor: category==='공부' &&'white', color:category==='공부' &&'black'}} onClick={onClickCategory}>공부</button>
+                <button className='category' style={{backgroundColor: category==='업무' &&'white', color:category==='업무' &&'black'}} onClick={onClickCategory}>업무</button>
+                <button className='category' style={{backgroundColor: category==='회의' &&'white', color:category==='회의' &&'black'}} onClick={onClickCategory}>회의</button>
             </div>
             <h2 className='main_title'>Model</h2>
             <ul className='model_list' style={{padding:0}}>
